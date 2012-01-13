912bf718a3a9b1e467aac0818ff38a508eec3ee4
Raynos <raynos2@gmail.com>
2012-01-13 19:38:21 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NodeSet
+
+Allowing you to operate on nodeLists as if they are a single node.
---
+++ test/nodeset-test.js
@@ -0,0 +1,69 @@
+suite("NodeSet", function () {
+    var nodes = "<div class='foo'></div> \
+                 <div class='foo'></div> \
+                 <div class='foo'></div>";
+
+    var testBody = document.getElementById("testbody"),
+        $ = NodeSet.$;
+
+    setup(function () {
+        var fragment = Fragment(nodes);
+        testBody.appendChild(fragment);
+    });
+
+    teardown(function () {
+        testBody.textContent = "";
+    });
+
+    suite("classList", function () {
+        test("classList add works", function () {
+            $(".foo").classList.add("bar");
+            assert($(".bar").length === 3, 
+               "not enough bar nodes");
+        });
+
+        test("classList remove works", function () {
+            $(".foo").classList.add("bar");
+            $(".foo").classList.remove("bar");
+            assert($(".bar").length === 0,
+                "too many bar nodes");
+        });
+
+        test("classList toggle works", function () {
+            $(".foo").classList.toggle("bar");
+            assert($(".bar").length === 3, 
+               "not enough bar nodes"); 
+            $(".foo").classList.toggle("bar");
+            assert($(".bar").length === 0,
+                "too many bar nodes"); 
+        });
+    });
+
+    suite("style", function () {
+        test("style works", function () {
+            $(".foo").style.color = 'red';
+            $(".foo").forEach(function (el) {
+                assert(el.style.color === 'red',
+                    "style was not set properly");
+            });
+        });
+    });
+
+    suite("Array", function () {
+        test("nodeset can be an array", function () {
+            var foos = $(".foo");
+            assert(foos.length, "does not have length");
+            assert(foos.forEach, "does not have forEach");
+            assert(foos.map, "does not have map");
+        });
+    });
+
+    function Fragment(html) {
+        var div = document.createElement("div"),
+            fragment = document.createDocumentFragment();
+
+        div.innerHTML = html;
+        while (div.hasChildNodes()) fragment.appendChild(div.firstChild);
+        return fragment;
+    }
+});
